ab7f0633649035e625e973e41603b3383d70ab69
LuisDio <luis.diolindo@gmail.com>
2019-11-06 14:23:48 +0100

    initiate connection

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Non-Ethereum browser detected. You should consider trying Metamask')
+    }
+  }
+
+  async loadBlockchainData(){
+
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +34,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            Dapp University Blockchain MarketPlace
           </a>
         </nav>
         <div className="container-fluid mt-5">
