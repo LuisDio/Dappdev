372b2d526e79552f0def7df7b85c7bea7dc70ba1
LuisDio <luis.diolindo@gmail.com>
2019-11-07 00:43:04 +0100

    refactoring with different component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import logo from '../logo.png';
 import './App.css';
 import Web3 from 'web3';
 import Marketplace from '../abis/Marketplace.json'
+import Navbar from './Navbar'
 
 class App extends Component {
 
@@ -53,21 +54,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University Blockchain MarketPlace
-          </a>
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-                <small className="text-white"><span id="account">{this.state.account}</span></small>
-            </li>
-          </ul>
-        </nav>
+        <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
