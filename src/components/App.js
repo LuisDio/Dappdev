ae47852c91c7c1f065ba9affabf099802b81b5ab
LuisDio <luis.diolindo@gmail.com>
2019-11-08 14:03:06 +0100

    Fetch product from blockchain and idisplay

--- src/components/App.js
+++ src/components/App.js
@@ -35,8 +35,18 @@ class App extends Component {
       const marketplace = web3.eth.Contract(Marketplace.abi, networkData.address)
       this.setState({ marketplace})
       const productCount = await marketplace.methods.productCount().call()
-      console.log(productCount.toString())
+      this.setState({ productCount})
+
+      // Fetch product from the blockchain
+      // Load product
+      for(var i = 1; i < productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
       this.setState({ loading: false})
+      console.log(this.state.products)
     } else {
       window.alert('Marketplace contract not deployed to detected network. ')
     }
@@ -74,7 +84,9 @@ class App extends Component {
             <main role="main" className="col-lg-12 d-flex">
               { this.state.loading 
                 ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div> 
-                : <Main createProduct={this.createProduct} /> 
+                : <Main 
+                  products={this.state.products} 
+                  createProduct={this.createProduct} /> 
               }
             </main>
           </div>
